d4618072899d4dfdad2ded064407c70b6908b66d
Nikhil Nagpal <nikhilnagpal656@gmail.com>
2025-10-23 22:34:27 +0530

    Update socket.js

--- Backend/socket.js
+++ Backend/socket.js
@@ -6,7 +6,7 @@ const server = http.createServer(app)
 
 const io = new Server(server , {
     cors : {
-        origin : "https://instavibe-tr5v.onrender.com",
+        origin : "http://localhost:5173",
         methods : ["GET" ,"POST"]
     }
 })
