ee9b4da510cb91e27690110c727ab76f7c8355b7
Nikhil Nagpal <nikhilnagpal656@gmail.com>
2025-10-23 23:14:11 +0530

    Update Reel.jsx

--- Frontend/src/pages/Reel.jsx
+++ Frontend/src/pages/Reel.jsx
@@ -8,13 +8,13 @@ const Reel = () => {
   const {reelData} = useSelector((state)=> state.reelSlice)
   return (
     <div className='w-full h-screen flex justify-center items-center bg-gray-900 overflow-hidden'>
-      <div className='flex fixed top-[20px] z-100 text-white text-2xl gap-4 items-center left-[20px]'>
+      <div className='flex fixed top-[20px] z-10 text-white text-2xl gap-4 items-center left-[20px]'>
         <Link to={"/"}>
           <FaArrowLeftLong />
         </Link>
         <p>Reels</p>
       </div>
-      <div className='w-full h-[100vh] snap-y snap-mandatory overflow-y-scroll'>
+      <div className='w-full h-[100vh] z-0 snap-y snap-mandatory overflow-y-scroll'>
       {
         reelData.map((reel)=>(
           <div className='h-screen snap-start'>
